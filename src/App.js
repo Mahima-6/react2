5fbd2686a79d74225f879347f592c56803b70cbd
Mahima-6 <mahimamodhawala@gmail.com>
2022-01-03 08:25:48 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 const { useState } = require("react");
 
 export default function App() {
-  let [counter, setcounter] = useState(60);
-  const incre = () => setcounter(counter + 1);
-
-  const decre = () => setcounter(counter - 1);
-
   return (
     <div>
-      <div>{counter}</div>
-      <div>
-        <input type="button" value="incre" onClick={incre} />
-        <input type="button" value="decre" onClick={decre} />
-      </div>
+      <h3>"Mahima Modhawala"</h3>
     </div>
   );
 }
