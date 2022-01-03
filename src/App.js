6472d88d785ad16b0ade4c4a0fc3655d35f4c83f
Mahima-6 <mahimamodhawala@gmail.com>
2022-01-03 11:22:18 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
-
-const { useState } = require("react");
+// export default function App() {
+//   return (
+//     <div>
+//       <div style={{ color: "red", backgroundColor: "royalblue" }}>Mahima</div>
+//     </div>
+//   );
+// }
 
 export default function App() {
   return (
-    <div>
-      <div className="beautify">"Mahima Modhawala"</div>
+    <div className="container-fluid">
+      <div className="row bg-dark text-light p-3">
+        <div className="col h2">React</div>
+      </div>
+      <div className="row">
+        <div className="col-12 bg-secondary text-light">Lorem10</div>
+      </div>
     </div>
   );
 }
