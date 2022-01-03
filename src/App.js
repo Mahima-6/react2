ccd73e3bb68ae6cb08000e08f03238deebf75894
Mahima-6 <mahimamodhawala@gmail.com>
2022-01-03 07:35:02 +0530

    like button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+const { useState } = require("react");
+
 export default function App() {
-  let counter = 100;
-  let username = "mahima";
+  let [counter, setcounter] = useState(60);
+  const incre = () => setcounter(counter + 1);
+
+  const decre = () => setcounter(counter - 1);
+
   return (
     <div>
       <div>{counter}</div>
-      <div>{username}</div>
+      <div>
+        <input type="button" value="incre" onClick={incre} />
+        <input type="button" value="decre" onClick={decre} />
+      </div>
     </div>
   );
 }
